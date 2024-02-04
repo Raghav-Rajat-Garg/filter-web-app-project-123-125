921faaefdd87056f1028762632f394f5a6242eda
Raghav-Rajat-Garg <130475644+Raghav-Rajat-Garg@users.noreply.github.com>
2024-02-04 20:31:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+noseX = 0;
+noseY= 0;
 function preload(){
-
+	mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 function setup(){
-	canvas = createCanvas(300, 300);
+	canvas = createCanvas(500, 500);
 	canvas.center(); 
 	video = createCapture(VIDEO);
-	video.size(300,300);
+	video.size(500,500);
 	video.hide();
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses)
@@ -14,7 +16,8 @@ function modelLoaded(){
 	console.log('hello posenet!!')
 }
 function draw(){
-
+	image(video,0,0,500,500);	
+	image(mustache,noseX-17,noseY,43,43)
 }
 function gotPoses(results){
 	if(results.length > 0){
